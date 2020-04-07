35d5bfbc17c7e863560350f833ab07b7912d869d
xiuye <xiuye_engineer@outlook.com>
2020-04-07 16:41:39 +0800

    android utilities

--- app/src/main/java/com/xy/activity/MainActivity.java
+++ app/src/main/java/com/xy/activity/MainActivity.java
@@ -5,11 +5,28 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.xiuye.util.cls.XType;
+import com.xiuye.util.log.XLog;
 import com.xy.lifecycle.MainActivityLifecycle;
 import com.xy.util.UIUtil;
+
+import java.util.Map;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.QueryMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,6 +60,65 @@ public class MainActivity extends AppCompatActivity {
             UIUtil.log(this, "item clicked", parent, v, pos, id);
         });
 
+        RadioGroup sexRadioGroup = findViewById(R.id.sexRadioGroup);
+
+        findViewById(R.id.fire).setOnClickListener(v -> {
+            int checkedButtonId = sexRadioGroup.getCheckedRadioButtonId();
+            if (checkedButtonId == -1) {
+                UIUtil.log(this, "please select!");
+
+            } else {
+                RadioButton checkedButton = findViewById(checkedButtonId);
+                String sex = checkedButton.getText().toString();
+                //http://myptcc.com/Api/Myptcc/setUserInfo
+                // ?
+                // value=1&user_id=793&field=sex
+                int sexId = 1;//man
+//                if("男".equals(sex)){
+//
+//                }else
+                if ("女".equals(sex)) {
+                    sexId = 0;
+                }
+
+                HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+                interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+                OkHttpClient.Builder client = new OkHttpClient.Builder();
+                client.addInterceptor(interceptor);
+
+
+                Retrofit retrofit = new Retrofit.Builder()
+                        .client(client.build())
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                        .baseUrl("http://myptcc.com")
+                        .build();
+                RequestService rs = retrofit.create(RequestService.class);
+                Map<String, Object> params = XType.map();
+                params.put("value", sexId);
+                params.put("user_id", 793);
+                params.put("field", "sex");
+                Observable<Map<String, Object>> result = rs.setUserIinfo(params);
+                result
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(d -> {
+                            XLog.log(d);
+                            UIUtil.log(d.toString());
+                            this.finish();
+                        });
+
+
+            }
+        });
+
     }
+
+}
+
+interface RequestService {
+    @GET("Api/Myptcc/setUserInfo")
+    Observable<Map<String, Object>> setUserIinfo(@QueryMap Map<String, Object> params);
 
 }

9254d7ef4564f4171890aeabae134a45882200f0
xiuye <xiuye_engineer@outlook.com>
2020-04-11 15:03:08 +0800

    components

--- app/src/main/java/com/xy/activity/ComponentsActivity.java
+++ app/src/main/java/com/xy/activity/ComponentsActivity.java
@@ -1,11 +1,14 @@
 package com.xy.activity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.widget.ProgressBar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.xiuye.util.cls.XType;
+import com.xy.util.UIUtil;
 
 public class ComponentsActivity extends AppCompatActivity {
 
@@ -17,10 +20,39 @@ public class ComponentsActivity extends AppCompatActivity {
             if (v instanceof ProgressBar) {
                 ProgressBar p = XType.cast(v);
                 int progress = p.getProgress();
-                progress += 10;
-                p.setProgress(progress);
+                if (progress < p.getMax()) {
+                    progress += 10;
+                    p.setProgress(progress);
+                } else {
+                    UIUtil.log("arrival max value: ",
+                            p.getMax() + ";",
+                            "now value: ",
+                            progress + ";");
+                }
+
 
             }
         });
+        findViewById(R.id.alertDialogBtn).setOnClickListener(v -> {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            dialog.setTitle("This is Dialog");
+            dialog.setMessage("Something important.");
+            dialog.setCancelable(false);
+            dialog.setPositiveButton("OK", (d, which) -> {
+
+            }).setNegativeButton("Cancel", (d, which) -> {
+
+            });
+            dialog.show();
+        });
+
+        findViewById(R.id.progressDialogBtn).setOnClickListener(v -> {
+            ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("This is ProgressDialog");
+            progressDialog.setMessage("Loading...");
+            progressDialog.setCancelable(true);
+            progressDialog.show();
+        });
+
     }
 }

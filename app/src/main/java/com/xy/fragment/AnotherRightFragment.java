fea761f163b7e760c61d9c1443881fa76df2bfb3
xiuye <xiuye_engineer@outlook.com>
2020-04-17 11:26:10 +0800

    Fragment lifetime

--- app/src/main/java/com/xy/fragment/AnotherRightFragment.java
+++ app/src/main/java/com/xy/fragment/AnotherRightFragment.java
@@ -1,5 +1,6 @@
 package com.xy.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.xiuye.util.log.XLog;
 import com.xy.activity.R;
 
 
@@ -16,6 +18,56 @@ public class AnotherRightFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        XLog.lg("AnotherRightFragment::onCreate");
         return inflater.inflate(R.layout.another_right_layout, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        XLog.lg("AnotherRightFragment::onCreate");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        XLog.lg("AnotherRightFragment::onStart");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        XLog.lg("AnotherRightFragment::onStop");
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        XLog.lg("AnotherRightFragment::onDestroy");
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        XLog.lg("AnotherRightFragment::onAttach");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        XLog.lg("AnotherRightFragment::onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        XLog.lg("AnotherRightFragment::onPause");
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        XLog.lg("AnotherRightFragment::onActivityCreated");
     }
 }
--- app/src/main/java/com/xy/fragment/RightFragment.java
+++ app/src/main/java/com/xy/fragment/RightFragment.java
@@ -1,5 +1,6 @@
 package com.xy.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,14 +10,64 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.xiuye.util.log.XLog;
 import com.xy.activity.R;
 
 public class RightFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
+        XLog.lg("RightFragment::onCreateView");
         return inflater.inflate(R.layout.right_layout, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        XLog.lg("RightFragment::onCreate");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        XLog.lg("RightFragment::onStart");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        XLog.lg("RightFragment::onStop");
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        XLog.lg("RightFragment::onDestroy");
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        XLog.lg("RightFragment::onAttach");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        XLog.lg("RightFragment::onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        XLog.lg("RightFragment::onPause");
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        XLog.lg("RightFragment::onActivityCreated");
     }
 }
 
